facdf8894089250878ae9751dcd9e304c85731f8
Sumonta Garai <sumontagarai2002@gmail.com>
2024-09-13 00:28:18 +0530

    completed

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-    function navanimation(){
+ function locomotiveAnimation(){
+    gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+
+  // for tablet smooth
+  tablet: { smooth: true },
+
+  // for mobile
+  smartphone: { smooth: true }
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+});
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+ }
+ 
+ function navanimation(){
         let nav= document.querySelector("nav");
         nav.addEventListener("mouseenter",function(){
             tl=gsap.timeline()
@@ -33,4 +70,114 @@
             })
         })
     }
-    navanimation()
+   
+    function loadingAnimation() {
+
+        var tl = gsap.timeline()
+        tl.from("#page1", {
+            opacity: 0,
+            duration: 0.2,
+            delay: 0.2
+        })
+        tl.from("#page1", {
+            transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+            borderRadius: "150px",
+            duration: 2,
+            ease: "expo.out"
+        })
+        tl.from("nav", {
+            opacity: 0,
+            delay: -0.2
+        })
+        tl.from("#page1 h1, #page1 p, #page1 div", {
+            opacity: 0,
+            duration: 0.5,
+            stagger: 0.2
+        })
+    }
+
+   function page2Animation() {
+    var rightElems = document.querySelectorAll(".right-elem")
+
+    rightElems.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 1,
+                scale: 1
+            })
+        })
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 0,
+                scale: 0
+            })
+        })
+        elem.addEventListener("mousemove", function (dets) {
+
+            gsap.to(elem.childNodes[3], {
+                x: dets.x - elem.getBoundingClientRect().x - 90,
+                y: dets.y - elem.getBoundingClientRect().y - 215
+            })
+        })
+    })
+    }
+
+   function page3videoanimation(){
+    var page3 = document.querySelector("#page3-center")
+
+    var video = document.querySelector("#page3 video")
+
+    page3.addEventListener("click",function(){
+        video.play()
+        gsap.to(video,{
+            transform: "scaleX(1) scaleY(1)",
+            opacity:1,
+            borderRadius: 0
+        })
+    })
+    video.addEventListener("click",function(){
+        video.pause()
+        gsap.to(video,{
+            transform: "scaleX(0.7) scaleY(0)",
+            opacity:0,
+            borderRadius: "30px"
+        })
+    })
+   }
+
+   function page5animation(){
+    var section = document.querySelectorAll("#page5-right")
+    section.forEach(function(elem){
+        elem.addEventListener("mouseenter",function(){
+            elem.childNodes[3].style.opacity=1
+            elem.childNodes[3].play();
+        })
+        elem.addEventListener("mouseleave",function(){
+            elem.childNodes[3].style.opacity=0
+            elem.childNodes[3].load()
+        })
+    })
+   }
+
+   function page7Animations() {
+    gsap.from(".btm6-parts h4", {
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "#btm6-part2",
+            scroller: "#main",
+            start: "top 80%",
+            end: "top 10%",
+            scrub: true
+        }
+    })
+   }
+
+
+locomotiveAnimation()
+loadingAnimation()
+navanimation()
+page2Animation()
+page3videoanimation()
+page5animation()   
+page7Animations() 
